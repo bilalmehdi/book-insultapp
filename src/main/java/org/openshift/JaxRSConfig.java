534a380d2059a63027b6affe915ac739c01a50e0
bilalmehdi <smehdi@smehdi.advent.com>
2016-09-02 16:00:54 -0700

    Renamed

--- src/main/java/org/openshift/JaxRSConfig.java
+++ src/main/java/org/openshift/JaxRSConfig.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class JaxrsConfig extends Application{
+public class JaxRSConfig extends Application{
 
 
 }
